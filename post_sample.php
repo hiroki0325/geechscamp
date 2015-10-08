0eec19083f4959472224368c4b0e39fcda8d153c
hiroki315 <nexseed.shimada@gmail.com>
2015-10-08 11:00:30 +0900

    day11

---
+++ post_sample.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ポストサンプル</title>
+</head>
+<body>
+  <h1>ポストサンプル</h1>
+  <form action="answer.php" method="post">
+    <div><input type="text"></div>
+    <div><input type="text"></div>
+    <div><input type="submit"></div>
+  </form>
+</body>
+</html>
