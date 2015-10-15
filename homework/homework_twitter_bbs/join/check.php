e851aa59b54d28f42b5b540421722582c5c6e585
hiroki315 <nexseed.shimada@gmail.com>
2015-10-15 22:25:53 +0900

    day15

---
+++ homework/homework_twitter_bbs/join/check.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>確認画面</title>
+</head>
+<body>
+  <form action="" method="post">
+    <dl>
+      <dt>ニックネーム</dt>
+      <dd>
+      </dd>
+      <dt>メールアドレス</dt>
+      <dd>
+      </dd>
+      <dt>パスワード</dt>
+      <dd>
+        【表示されません】
+      </dd>
+      <dt>写真など</dt>
+      <dd>
+      </dd>
+    </dl>
+    <div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a>｜<input type="submit" value="登録する"></div>
+  </form>
+</body>
+</html>
