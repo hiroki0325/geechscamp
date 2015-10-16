48805904a01414faa762edcb80c447cee3deebcb
hiroki315 <nexseed.shimada@gmail.com>
2015-10-16 13:59:49 +0900

    day18

--- twitter_bbs/join/index.php
+++ twitter_bbs/join/index.php
@@ -8,11 +8,13 @@
 
     // empty関数
     //// 空だったらtrueを返す
+
     //// isset関数の逆バージョン
     // !の機能
     //// 関数等の前に付けてif文の条件に入れるとtrueもしくはfalseを逆に変換します
     if (!empty($_POST)) {
-      // エラー項目の確認
+
+      // エラー項目の確認//
       if ($_POST["nickname"] == '') {
         $error["nickname"] = 'blank';
       }
@@ -23,15 +25,32 @@
       // strlen関数
       //// 文字の長さを検証する関数
       if (strlen($_POST["password"]) < 4) {
-        $error["password"] = 'lengh';
+        $error["password"] = 'length';
       }
 
       if ($_POST["password"] == '') {
         $error["password"] = 'blank';
       }
 
+      $fileName = $_FILES["image"]["name"];
+      if (!empty($fileName)){
+        // substr関数
+        // 第一引数に入れた文字列から第二引数に入れた文字数から始まる文字を取得
+        $ext = substr($fileName, -3);
+          if ($ext != 'jpg' && $ext != 'gif') {
+              $error["image"] = "type";
+          }
+      }
+      ////////////////////////////////
+
+      // エラーがなければ次のページに行くための処理
       if (empty($error)) {
+        // 画像アップロード
+        $image = date('YmdHis') . $_FILES["image"]["name"];
+        move_uploaded_file($_FILES["image"]["tmp_name"], '../member_picture/'.$image);
+
         $_SESSION["join"] = $_POST;
+        $_SESSION["join"] ["image"]= $image;
 
         header('Location: check.php');
         exit();
@@ -78,15 +97,54 @@
 
     <div>
       <label for="">メールアドレス</label>
-      <input type="text" name="email">
-    </div, ENT_QUOTES, 'UTF-8'>
+      <?php
+          if (isset($_POST["email"])) {
+            echo sprintf('<input type="text" name="email" value="%s">',
+               h($_POST["email"])
+               );
+          } else {
+            echo '<input type="text" name="email">';
+          }
+       ?>
+       <?php if (isset($error["email"])): ?>
+            <?php if ($error["email"] == 'blank'): ?>
+                <p class="error">* メールアドレスを入力してください</p>
+            <?php endif ;?>
+        <?php endif; ?>
+    </div>
     <div>
       <label for="">パスワード</label>
-      <input type="password" name="password">
+      <?php
+          if (isset($_POST["password"])) {
+            echo sprintf('<input type="password" name="password" value="%s">',
+               h($_POST["password"])
+               );
+          } else {
+            echo '<input type="password" name="password">';
+          }
+       ?>
+       <?php if (isset($error["password"])): ?>
+            <?php if ($error["password"] == 'blank'): ?>
+                <p class="error">* パスワードを入力してください</p>
+            <?php endif ;?>
+            <?php if ($error["password"] == 'length'): ?>
+                <p class="error">* パスワードは4文字以上で入力してください</p>
+            <?php endif ;?>
+        <?php endif; ?>
+
     </div>
 
     <div>
       <label for="">プロフィール画像</label>
+      <?php if (isset($error["image"])): ?>
+           <?php if ($error["image"] == 'type'): ?>
+               <p class="error">* 画像形式はjpgもしくはgifを指定してください。</p>
+           <?php endif ;?>
+       <?php endif; ?>
+       <?php if (!empty($error)): ?>
+           <p class="error">* 恐れ入りますが、画像を改めて指定してください。</p>
+       <?php endif ;?>
+
       <input type="file" name="image">
     </div>
 
