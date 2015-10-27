ca0ee8a2d7a6a99a1a340aed2ed124f1a2759de1
hiroki315 <nexseed.shimada@gmail.com>
2015-10-27 13:11:15 +0900

    day27

--- twitter_bbs/index.php
+++ twitter_bbs/index.php
@@ -26,7 +26,7 @@
   // 投稿を記録する
   if (!empty($_POST)) {
     if ($_POST['message'] != '') {
-      $sql = sprintf('INSERT INTO posts SET member_id=%d, message="%s", reply_post_id=%d,created=NOW()',
+      $sql = sprintf('INSERT INTO posts SET member_id=%d, message="%s", reply_post_id=%d, created=NOW()',
         mysqli_real_escape_string($db, $member['id']),
         mysqli_real_escape_string($db, $_POST['message']),
         mysqli_real_escape_string($db, $_POST['reply_post_id'])
@@ -40,7 +40,48 @@
   }
 
   // 投稿を取得する
-  $sql = 'SELECT m.nickname, m.picture, p.* FROM members m, posts p WHERE m.id = p.member_id ORDER BY p.created DESC';
+  //// URLに?page=2などのページ番号があれば、それを取得して$pageに代入
+  $page = $_REQUEST['page'];
+  if ($page == '') {
+    ////// URLに?pageがなければ1ページ目とみなし$pageに1を代入します
+    $page = 1;
+  }
+
+  //// max関数
+  //// ()内に指定した複数データから一番大きい値を取得する
+  //// もし、ユーザーがURLに?page=0.8のような値を入れてリクエストした場合に、
+  //// 強制的に1ページ目に飛ぶようにしている
+  $page = max($page, 1);
+
+  // 最終ページを取得する
+  //// SELECT COUNTを使ってpostsテーブルのデータの件数を取得
+  $sql = 'SELECT COUNT(*) AS cnt FROM posts';
+
+  $recordSet = mysqli_query($db, $sql) or die(mysqli_error($db));
+  $table = mysqli_fetch_assoc($recordSet);
+
+  //// ceil関数
+  //// 小数点以下切り上で数字を作る 例：1.8が指定された場合は切り上げて2を返す
+  $maxPage = ceil($table['cnt'] / 5); // ←5件がマックスで表示したいデータ件数のため5で割る
+
+  // もし、ユーザーがURLに?page=100などのような大きすぎる値を入れてリクエストを送ってきた場合に、
+  // DBに保存されているデータの件数を5で割り最大ページ数を算出し、
+  // もしそれ以上の値がセットされていた場合はmin関数を使用して最大ページ数で表示する
+
+  //// min関数
+  //// ()内に指定した複数データから一番小さい値を取得する
+  $page = min($page, $maxPage);
+
+  // 1ページ目なら$startなら0が代入され、DBからSELECT ~ LIMIT 0,5 とすることで、
+  // 1個目のデータ(idが1のもの)から5件取得するための$startを用意
+  // もししていされたページが2ページ目なら、$pageには2が入り、計算処理の結果$startには5が入ります。
+  // その後SELECT ~ LIMIT 5,5 というsql文が発行され、6個目のデータから5件取得する処理が作られます。
+  $start = ($page-1) * 5;
+  $start = max(0, $start);
+
+  $sql = sprintf('SELECT m.nickname, m.picture, p.* FROM members m, posts p WHERE m.id = p.member_id ORDER BY p.created DESC LIMIT %d, 5',
+    $start
+    );
 
   $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
   // var_dump($posts);
@@ -99,7 +140,9 @@
 
       </p>
       <p class="day">
-        <?php echo h($post['created']); ?>
+        <a href="view.php?id=<?php echo h($post['id']); ?>">
+         <?php echo h($post['created']); ?>
+        </a>
         <?php if ($_SESSION['id'] == $post['member_id']): ?>
           <!-- $_SESSION['id']がログイン中のユーザーのID -->
           <!-- $post['member_id']が表示される投稿データ一件に登録されている投稿者のID -->
